2a40a3a61204b0ac8192ef703d90e688d43443a1
jshields-ca <jeremy.shields@gmail.com>
2025-08-12 13:40:31 -0500

    security: add HSTS and security headers via Next headers()

--- next.config.js
+++ next.config.js
@@ -19,6 +19,20 @@ const nextConfig = {
           },
         ],
       },
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains; preload',
+          },
+          { key: 'X-Content-Type-Options', value: 'nosniff' },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
+        ],
+      },
     ]
   },
   experimental: {
